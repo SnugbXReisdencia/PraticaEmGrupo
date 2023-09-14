e5e9988d056041573a1401d3f120fd7383bba87a
mikaelleRubia <mikaelle.rubia@outlook.com>
2023-09-14 11:30:35 -0300

    feature: estrutura de como fazer

--- index.cpp
+++ index.cpp
@@ -13,10 +13,13 @@ int main() {
         int qtd[100];
     };
     
-    // Produto p1{ "teste", 5};
-
-    // cout << "produto " << p1.produto << p1.qtd<< endl;
-
+    // criar função dentro do struct, 
+    //     struct Produto{
+    //     string produto[100];
+    //     int qtd[100];
+    //     void add_procuto(nome, qtd );
+    // produto1:: add_procuto(nome, qtd );
+    // };
 
     return 0;
 }
