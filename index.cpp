653677419e37c09c290eaeaba6872c6eac541913
mikaelleRubia <mikaelle.rubia@outlook.com>
2023-09-14 11:26:27 -0300

    feature: struct produto

---
+++ index.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+
+using namespace std;
+
+int main() {
+    vector<string> vec;
+
+    struct Produto{
+        string produto[100];
+        int qtd[100];
+    };
+    
+    // Produto p1{ "teste", 5};
+
+    // cout << "produto " << p1.produto << p1.qtd<< endl;
+
+
+    return 0;
+}
