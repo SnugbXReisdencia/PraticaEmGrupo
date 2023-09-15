4adefd2c8de76cd7c01d884999a2d20785e98da4
mikaelleRubia <mikaelle.rubia@outlook.com>
2023-09-15 18:55:46 -0300

    fix: função add_produto

--- add_produto.cpp
+++ add_produto.cpp
@@ -1,60 +1,54 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 using namespace std;
+struct Produto{
+    string produto;
+    int qtd=0;
+    double preco=0.0;
+
+};
+
+void add_produto( std::vector<Produto>& produtos){
+        Produto novo_produto;
+
+        char resposta;
+        int cont = 0;
+
+        do{
+            cont++;
+            std::cout << " Digite o nome do produto: " << endl;
+            cin>> novo_produto.produto;
+
+            cout << "Preço do produto: " << endl;
+            cin>> novo_produto.preco;
+
+            cout << "Quantidade do produto: " << endl;
+            cin>> novo_produto.qtd;
+
+            produtos.push_back(novo_produto);
+
+            cout << " Deseja Adicionar outro produto (s/n)? " << endl;
+            cin>> resposta;
+
+        }while(resposta == 's');    
+
+        // for para teste 
+        // for (int i = 0; i < produtos.size(); i++) {
+        //         cout << "Produto           Preço        Quantidade " << endl;
+        //         cout << produtos[i].produto << "              " << produtos[i].preco << "           " << produtos[i].qtd  << endl;
+        //         cout << "====================================================================" << endl;
+        //     }
+
+};
+
 
 int main() {
 
-    string inseri_produto, resposta;
-    int inseri_qtd;
-    double inseri_preco;
-    int cont=0;
+    vector <Produto> produtos;
 
-
-    struct Produto{
-        string produto;
-        int qtd=0;
-        double preco=0.0;
-
-        void add_produto(string produto_novo, int qtd_novo, double preco_novo){
-            produto = produto_novo;
-            qtd = qtd_novo;
-            preco = preco_novo;
-            }
-    };
-
-    Produto Produto_[10];
-    int tamanha_vetor = sizeof(Produto_) / sizeof(Produto_[0]);
-
-    for (int i = 0; i <= tamanha_vetor-1; i++) {
-        cont ++;
-        cout << " Digite o nome do produto: " << endl;
-        cin>> inseri_produto;
-
-        cout << "Preço do produto: " << endl;
-        cin>> inseri_preco;
-
-        cout << "Quantidade do produto: " << endl;
-        cin>> inseri_qtd;
-
-        Produto_[i].add_produto(inseri_produto, inseri_qtd, inseri_preco);
-
-        if(cont < tamanha_vetor){
-            cout << " Deseja Adicionar outro produto (sim)? " << endl;
-            cin>> resposta;
-        }
-
-        if(resposta != "sim"){
-            break;
-        }
-       
-    }
-
-    for (int i = 0; i < cont; i++) {
-        cout << "Produto           Preço        Quantidade " << endl;
-        cout << Produto_[i].produto << "              " << Produto_[i].preco << "           " << Produto_[i].qtd  << endl;
-        cout << "====================================================================" << endl;
-    }
+    add_produto(produtos);
 
     return 0;
 }
